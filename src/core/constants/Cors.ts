91f9f76707261a4d475693f6a71edd97772593b6
unknown <joakig6@gmail.com>
2023-08-19 14:36:42 -0300

    prepare to deploy

--- src/core/constants/Cors.ts
+++ src/core/constants/Cors.ts
@@ -2,8 +2,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 
 export const CORS : CorsOptions ={
-      origin: [process.env.URL_FRONT],
-      // origin: ['http://localhost:3000', process.env.URL_FRONT],
+      origin: ['http://localhost:3000', process.env.URL_FRONT],
     allowedHeaders: ['Content-Type', 'Authorization',],
     methods: 'GET,PUT,POST,DELETE,OPTIONS,HEAD,PATCH',
     credentials:true,
